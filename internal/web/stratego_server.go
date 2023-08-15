f09cc01fa65b7b679644f3a3301286301285766b
Casey Boyer <casey.boyer@spoke-aiot.com>
2023-08-15 16:06:19 +0000

    simplify proto layout

--- internal/web/stratego_server.go
+++ internal/web/stratego_server.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	pb "github.com/cBiscuitSurprise/strate-go/api/go/strategopb"
-	game "github.com/cBiscuitSurprise/strate-go/api/go/strategopb/game"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -26,7 +25,7 @@ func (s *strateGoServer) DeepPing(ctx context.Context, _ *emptypb.Empty) (*pb.Po
 	return &pb.Pong{
 		Timestamp: &timestamppb.Timestamp{Seconds: time.Now().Unix()},
 		Message:   "pong",
-		Games:     []*game.Game{},
+		Games:     []*pb.Game{},
 	}, nil
 }
 
